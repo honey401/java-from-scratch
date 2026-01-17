07572228d4e30acde0b22e4bc5f6aeab413269b0
honey401 <hrudrapa@gitam.in>
2026-01-17 20:23:23 +0530

    Add EvenOdd.java to check even or odd numbers
    Added even odd program

---
+++ basics/EvenOdd.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void Main(String [] args) {
+    int num = 10;
+
+    if (num % 2 == 0) {
+      System.out.println("even");
+    } else {
+      System.out.println("odd");
+   }
+ }
+}
+
